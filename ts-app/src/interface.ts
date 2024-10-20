ca8dcdd8328917a23947bdeaa25335b0b4be9455
Barath <barathraj048@gmail.com>
2024-10-20 16:39:49 +0530

    add type

--- ts-app/src/interface.ts
+++ ts-app/src/interface.ts
@@ -25,11 +25,14 @@ class Employe implements Persion {
    name:string; //assigning types in class
    age:number
 
-   constructor (n:string,a:number){
+   constructor (n:string ,a:number){
       this.name=n;  //assigining constructors and its value
       this.age=a
    }
    Greet(msg: string): void {  //assigning function
-      console.log(`${this.name} ${this.age}`)
+      console.log(`${this.name} says ${msg}`)
    }
 }
+
+const barath = new Employe('barath',19)
+barath.Greet('hellow Forkes')
