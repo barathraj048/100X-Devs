2a925f93648aa8adf439d42a278e18dbe0172d10
Barath <barathraj048@gmail.com>
2024-10-20 11:44:39 +0530

    implement

---
+++ ts-app/src/interface.ts
@@ -0,0 +1,35 @@
+interface User {
+   name:string;
+   age:number;
+   email ?: string; //optionnal variable
+}
+
+const interface_pra=(User_details : User):void=> {
+
+}
+const user_det={
+   name:'barath',
+   age:19
+}
+interface_pra(user_det)
+
+//interfacing class
+
+interface Persion {
+   name: string;
+   age:number;
+   Greet(msg : string) : void
+}
+
+class Employe implements Persion {
+   name:string; //assigning types in class
+   age:number
+
+   constructor (n:string,a:number){
+      this.name=n;  //assigining constructors and its value
+      this.age=a
+   }
+   Greet(msg: string): void {  //assigning function
+      console.log(`${this.name} ${this.age}`)
+   }
+}
