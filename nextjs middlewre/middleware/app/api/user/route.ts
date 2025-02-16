8630d77427d2051d662c42e43bff5fa11961132e
Barath <barathraj048@gmail.com>
2025-02-16 21:46:26 +0530

    added midleware config

--- nextjs middlewre/middleware/app/api/user/route.ts
+++ nextjs middlewre/middleware/app/api/user/route.ts
@@ -1,9 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET=async(req:NextRequest)=> {
-   return(
-      NextResponse.json({
-         message:"Hello from user route"
-      })
-   )
+let count=0
+export const GET = async (req: NextRequest) => {
+  if (req.nextUrl.pathname === "/api/secret") {
+    // Use new URL to create absolute URL for redirection
+    const url = new URL('/verify', req.url)
+    return NextResponse.redirect(url)
+  }
+  count++;
+  
+  console.log(`count from middleware: ${count}`) 
+  return NextResponse.next()
+}
+
+// Matcher should match the exact paths you want to handle
+export const config = {
+  matcher: [
+    '/api/:path*'
+  ]
 }
