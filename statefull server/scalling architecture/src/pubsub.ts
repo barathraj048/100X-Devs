f282d409f96fd165be41ac1ef6b03ac8eb1f5634
Barath <barathraj048@gmail.com>
2025-05-26 17:18:45 +0530

    added pubsubmanager in singleton partten

--- statefull server/scalling architecture/src/pubsub.ts
+++ statefull server/scalling architecture/src/pubsub.ts
@@ -1,23 +1,50 @@
-import { createClient } from 'redis';
+import { createClient,RedisClientType } from "redis";
+export class pubSubManager{
+   private static instence: pubSubManager;
+   private redisClient:RedisClientType;
+   private subscriber:Map<string,string[]>
+   constructor(){
+      this.redisClient= createClient();
+      this,this.redisClient.connect()
+      this.subscriber = new Map();
+   }
+   static getInstance(){
+      if(!pubSubManager.instence) {
+         pubSubManager.instence = new pubSubManager();
+      }
+      return pubSubManager.instence;
+   }
 
-const publisher = createClient({
-   socket: {
-    host: 'localhost', 
-    port: 6379,
-  }
-});
+   subscribe(userid:string,stock:string){
+      if(!this.subscriber.has(stock)){
+         this.subscriber.set(stock,[])
+      }
+      this.subscriber.get(stock)?.push(userid)
 
-let connect= async()=> {
-   try{
-      await publisher.connect();
-      console.log('Publisher connected to Redis');
-   }catch(err){
-      console.error('Error connecting to Redis:', err);
+      if(this.subscriber.get(stock)?.length ==1){
+         this.redisClient.subscribe(stock,(msg)=> {
+            this.handleMessage(stock,msg);
+         })
+         console.log(`Subscribed to ${stock} channel`);
+      }
+   }
+   unsubscribe(userid:string,stock:string){
+      this.subscriber.set(stock, this.subscriber.get(stock)?.filter((id) => id !== userid) || []);
+         if (this.subscriber.get(stock)?.length === 0) {
+            this.redisClient.unsubscribe(stock);
+            console.log(`UnSubscribed to Redis channel: ${stock}`);
+        }
+   }
+
+   handleMessage(stock:string,msg:string){
+      console.log(`message reciver for ${stock} is ${msg}`);
+      this.subscriber.get(stock)?.forEach(element => {
+         console.log(`sending message to users ${element}`)
+      });
+   }
+
+   public async disconnect(){
+      this.redisClient.quit()
+      console.log("Redis client disconnected");
    }
 }
-connect()
-setInterval(async () => {
-  const message = `Hello at ${new Date().toISOString()}`;
-  await publisher.publish('my-channel', message);
-  console.log('Published:', message);
-}, 2000);
