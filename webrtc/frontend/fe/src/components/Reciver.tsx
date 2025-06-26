27c33872080eb792f2acdadd5b4a77ff12526445
Barath <barathraj048@gmail.com>
2025-06-26 21:44:58 +0530

    simple webrtc done

--- webrtc/frontend/fe/src/components/Reciver.tsx
+++ webrtc/frontend/fe/src/components/Reciver.tsx
@@ -1,47 +1,57 @@
-import { useEffect } from "react"
-
+import { useEffect } from "react";
 
 export const Receiver = () => {
-    
     useEffect(() => {
         const socket = new WebSocket('ws://localhost:8080');
+
         socket.onopen = () => {
-            socket.send(JSON.stringify({
-                type: 'receiver'
-            }));
-        }
+            socket.send(JSON.stringify({ type: 'receiver' }));
+        };
+
         startReceiving(socket);
     }, []);
 
     function startReceiving(socket: WebSocket) {
         const video = document.createElement('video');
+        video.autoplay = true;
+        video.playsInline = true;
         document.body.appendChild(video);
 
         const pc = new RTCPeerConnection();
-        pc.ontrack = (event) => {
-            video.srcObject = new MediaStream([event.track]);
-            video.play();
-        }
 
-        socket.onmessage = (event) => {
-            const message = JSON.parse(event.data);
-            if (message.type === 'createOffer') {
-                pc.setRemoteDescription(message.sdp).then(() => {
-                    pc.createAnswer().then((answer) => {
-                        pc.setLocalDescription(answer);
-                        socket.send(JSON.stringify({
-                            type: 'createAnswer',
-                            sdp: answer
-                        }));
-                    });
-                });
-            } else if (message.type === 'iceCandidate') {
-                pc.addIceCandidate(message.candidate);
+        const mediaStream = new MediaStream();
+        video.srcObject = mediaStream;
+
+        pc.ontrack = (event) => {
+            mediaStream.addTrack(event.track);
+        };
+
+        pc.onicecandidate = (event) => {
+            if (event.candidate) {
+                socket.send(JSON.stringify({
+                    type: 'iceCandidate',
+                    candidate: event.candidate
+                }));
             }
-        }
+        };
+
+        socket.onmessage = async (event) => {
+            const message = JSON.parse(event.data);
+
+            if (message.type === 'createOffer') {
+                await pc.setRemoteDescription(message.sdp);
+                const answer = await pc.createAnswer();
+                await pc.setLocalDescription(answer);
+
+                socket.send(JSON.stringify({
+                    type: 'createAnswer',
+                    sdp: answer
+                }));
+            } else if (message.type === 'iceCandidate') {
+                await pc.addIceCandidate(message.candidate);
+            }
+        };
     }
 
-    return <div>
-        
-    </div>
-}
+    return <div>Receiver Ready</div>;
+};
--- webrtc/frontend/fe/src/components/Sender.tsx
+++ webrtc/frontend/fe/src/components/Sender.tsx
@@ -1,73 +1,65 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export const Sender = () => {
     const [socket, setSocket] = useState<WebSocket | null>(null);
-    const [pc, setPC] = useState<RTCPeerConnection | null>(null);
 
     useEffect(() => {
-        const socket = new WebSocket('ws://localhost:8080');
-        setSocket(socket);
-        socket.onopen = () => {
-            socket.send(JSON.stringify({
-                type: 'sender'
-            }));
-        }
+        const ws = new WebSocket('ws://localhost:8080');
+        ws.onopen = () => {
+            ws.send(JSON.stringify({ type: 'sender' }));
+        };
+        setSocket(ws);
     }, []);
 
     const initiateConn = async () => {
-
-        if (!socket) {
-            alert("Socket not found");
+        if (!socket || socket.readyState !== WebSocket.OPEN) {
+            alert("WebSocket is not connected");
             return;
         }
 
-        socket.onmessage = async (event) => {
-            const message = JSON.parse(event.data);
-            if (message.type === 'createAnswer') {
-                await pc.setRemoteDescription(message.sdp);
-            } else if (message.type === 'iceCandidate') {
-                pc.addIceCandidate(message.candidate);
-            }
-        }
-
         const pc = new RTCPeerConnection();
-        setPC(pc);
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+
+        // Attach local preview
+        const video = document.createElement('video');
+        video.srcObject = stream;
+        video.autoplay = true;
+        video.playsInline = true;
+        document.body.appendChild(video);
+
+        stream.getTracks().forEach(track => pc.addTrack(track, stream));
+
         pc.onicecandidate = (event) => {
             if (event.candidate) {
-                socket?.send(JSON.stringify({
+                socket.send(JSON.stringify({
                     type: 'iceCandidate',
                     candidate: event.candidate
                 }));
             }
-        }
+        };
+
+        socket.onmessage = async (event) => {
+            const message = JSON.parse(event.data);
+
+            if (message.type === 'createAnswer') {
+                await pc.setRemoteDescription(message.sdp);
+            } else if (message.type === 'iceCandidate') {
+                await pc.addIceCandidate(message.candidate);
+            }
+        };
 
         pc.onnegotiationneeded = async () => {
             const offer = await pc.createOffer();
             await pc.setLocalDescription(offer);
-            socket?.send(JSON.stringify({
+            socket.send(JSON.stringify({
                 type: 'createOffer',
-                sdp: pc.localDescription
+                sdp: offer
             }));
-        }
-            
-        getCameraStreamAndSend(pc);
-    }
-
-    const getCameraStreamAndSend = (pc: RTCPeerConnection) => {
-        navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-            const video = document.createElement('video');
-            video.srcObject = stream;
-            video.play();
-            // this is wrong, should propogate via a component
-            document.body.appendChild(video);
-            stream.getTracks().forEach((track) => {
-                pc?.addTrack(track);
-            });
-        });
-    }
+        };
+    };
 
     return <div>
         Sender
-        <button onClick={initiateConn}> Send data </button>
-    </div>
-}
+        <button onClick={initiateConn}>Send Video</button>
+    </div>;
+};
